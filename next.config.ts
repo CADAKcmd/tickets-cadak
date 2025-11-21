d1b76a2446b016128b873c0759ae30be6523532b
CADAKcmd <kianogee@gmail.com>
2025-11-21 12:18:48 +0100

    message

--- next.config.ts
+++ next.config.ts
@@ -6,17 +6,8 @@ const nextConfig: NextConfig = {
   },
   images: {
     qualities: [75, 85],
-  },
-};
-
-export default nextConfig;
-import type { NextConfig } from 'next';
-
-const nextConfig: NextConfig = {
-  images: {
     remotePatterns: [
       { protocol: 'https', hostname: 'i.pinimg.com', pathname: '/**' },
-      // add any others you might use:
       { protocol: 'https', hostname: 'images.unsplash.com', pathname: '/**' },
       { protocol: 'https', hostname: 'res.cloudinary.com', pathname: '/**' },
     ],
